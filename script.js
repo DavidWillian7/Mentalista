d1b81257e142fe5ae7047df9bcc7363d29ef4b41
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-03-26 13:08:05 -0300

    final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var numero = parseInt(Math.random() * 11);
+var tentativas = 0;
 
 function chutar(){
     let chute = parseInt(document.getElementById("valor").value);
@@ -7,6 +8,7 @@ function chutar(){
     if(chute == numero){
         resultado.innerHTML = "Parabéns. Você acertou o número secreto!";
         aux = numero;
+        tentativas = 0;
         numero = parseInt(Math.random() * 11);
         while(aux == numero){
             numero = parseInt(Math.random() * 11); 
@@ -15,7 +17,18 @@ function chutar(){
         resultado.innerHTML = "Erro. Você deve digitar um número de 0 a 10!"
     }else if(chute < numero){
         resultado.innerHTML = "Você errou, seu chute é MENOR que o número secreto!";
+        tentativas++;
     }else{
         resultado.innerHTML = "Você errou, seu chute é MAIOR que o número secreto!";
+        tentativas++;
+    }
+    if(tentativas === 3){
+        resultado.innerHTML = `Suas tentativas acabaram o número secreto era ${numero}.`;
+        aux = numero;
+        tentativas = 0;
+        numero = parseInt(Math.random() * 11);
+        while(aux == numero){
+            numero = parseInt(Math.random() * 11); 
+        }
     }
 }
